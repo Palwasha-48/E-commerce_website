92f8ea9e552ff8c379c8f7b969a9844d7443c7e8
Palwasheyyy <163187583+Palwasha-48@users.noreply.github.com>
2024-12-18 09:16:50 +0500

    Update and rename Footer.tsx to Home.tsx

---
+++ src/app/components/Home.tsx
@@ -0,0 +1,23 @@
+import Footer from "./Footer"
+import Navbar from "./Navbar";
+import Homebg from "./Homebg";
+import Homethree from "./Homethree";
+import Homepro from "./Homepro";
+import HomePics from "./HomePics";
+import HomeSlides from "./HomeSlides";
+
+export default function HomePage() {
+  return ( 
+    <>
+    <div className="bg-white min-h-screen pb-4">   
+    <Navbar />
+    <Homebg />
+    <Homethree />
+    <Homepro />
+    <HomeSlides />
+    <HomePics />
+    <Footer />
+</div>
+      </>
+)
+}
