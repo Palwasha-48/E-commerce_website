3425243bdcc55bfa5e26f78bd801c3b9cd212a1b
Palwasheyyy <163187583+Palwasha-48@users.noreply.github.com>
2024-12-18 09:26:42 +0500

    Update Page.tsx

--- shop/Page.tsx
+++ shop/Page.tsx
@@ -1,12 +1,1 @@
-import Navbar from "../components/Navbar";
 
-export default function shop(){
-    return(
-        <>
-        <div>
-            <Navbar />
-            
-        </div>
-        </>
-    )
-}
