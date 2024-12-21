587f69050c6e1bd78be6064d7b8003487334c12d
Palwasheyyy <163187583+Palwasha-48@users.noreply.github.com>
2024-12-21 19:35:57 +0500

    Update Home.tsx

--- src/app/components/Home.tsx
+++ src/app/components/Home.tsx
@@ -1,19 +1,20 @@
+import Footer from "./Footer"
+import Navbar from "./Navbar";
 import Homebg from "./Homebg";
 import Homethree from "./Homethree";
 import Homepro from "./Homepro";
-import HomePics from "./HomePics";
-import HomeSlides from "./HomeSlides";
+import Home_slides from "./Home_slides";
+import HomePics from "./Homepics";
 
 export default function HomePage() {
   return ( 
     <>
-  <div className="bg-white min-h-screen pb-4">   
+    <div className="bg-white min-h-screen pb-4">   
     <Homebg />
     <Homethree />
     <Homepro />
+    <Home_slides />
     <HomePics />
-    <HomeSlides />
-  </div>
+</div>
       </>
-)
-}
+      )}
